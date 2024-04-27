5a62c7c447fe182605eaa7fa4d7a20a317bdb56b
Simon H-M <simonhm31@gmail.com>
2024-04-27 14:43:29 -0500

    afln

--- index.js
+++ index.js
@@ -1,11 +1,118 @@
 const myName = 'Joe King';
 console.log(myName);
 
-let person = {
-    name: "Joe",
-    age: 21,
-    occupation: "Unemployed"
-}
+//Objects:
+    let person = {
+        name: "Joe",
+        age: 21,
+        occupation: "Unemployed"
+    }
 
-console.log(person);
-console.log(person.occupation);
+    //logging object to console
+    console.log(person);
+    //logging specific property of object
+    console.log(person.occupation);
+
+
+
+
+//Arrays
+    let anArray = ['this', 'is', 'an', 'array'];
+
+    console.log(anArray[1]); // logs 'is'
+
+    anArray[4] = 'newElement'; // appends new element to end of array
+    console.log(anArray[4]) //logs new element
+
+    // Arrays are technically Objects!!
+    console.log(typeof anArray); //returns "object"
+    
+    //Because arrays are objects, you can access properties of an array
+    console.log(anArray.length); //returns 5
+
+
+
+//Functions:
+    //function: set of statements that performs a task or calculates a value - mosh
+
+    function greet(firstName, lastName){
+        console.log("Hello " + firstName + " " + lastName);
+
+    }
+    greet("John", "Johnson");
+    greet("Joe", "Smith");
+
+    //In this case, "firstName" is a parameter for the greet() function. "John" is 
+    //NOT a parameter for the greet() function. John is an argument
+
+    //parameter: a variable that only exists inside of a function
+    //argument: the actual value that gets passed in when you call the function
+
+
+    function square(number) {
+        return number * number;
+    }
+
+    console.log(square(123123124.235235));
+
+
+//Operators:
+
+// There are 5 different types of Operators in JS
+//     Arithmetic Operators
+//     Assignment Operators
+//     Comparison Operators
+//     Logical Operators
+//     Bitwise Operators
+
+
+
+//Arithmetic Operators:
+let x = 400;
+let y = 20;
+
+console.log(x + y); //x plus y
+console.log(x - y); //x minus y
+console.log(x * y); //x times y
+console.log(x / y); // x divided by y
+console.log(x % y); // what is the remainder when x is divded by y
+console.log(x ** y); // x to the power of y
+
+//Increment (++)
+console.log(x++); // Logs x to the console, and THEN increments x. Returns 400
+console.log(++x); // Increments x, and THEN logs x to the console. Returns 402!
+
+
+//Decrement
+console.log(--y); // Decrements y, and THEN logs it to the console. Returns 19.
+console.log(y--); // Logs y to the console, and THEN decrements it. Returns 19.
+console.log(y); //Returns 18. 
+
+//Assignment Operators
+
+    //The "=" assignment operator assigns the value 10 to x
+    x = 10;
+    y = 5;
+
+    x = x + 5; // adding 5 to x
+    x += 5; // using the addition assignment operator to add 5 to x
+    console.log(x)  // returns 20;
+
+    //Similar to the addition assignment operator:
+
+    x -= 5;
+    console.log(x); //returns 15
+
+    x *= 5;
+    console.log(x); //returns 75
+
+    x /= 5; 
+    console.log(x); //returns 15
+
+    x %= 5; //Gets the remainder when x is divided by 5
+    console.log(x); //returns 0
+
+    x **= 5; // x to the power of 5, in this case x = 0 so 0 * 0 * 0 * 0 * 0
+    console.log(x) //Returns 0
+
+//Comparison Operators
