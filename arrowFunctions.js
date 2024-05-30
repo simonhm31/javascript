68f9818cca216e282c9ed1b8bbd322659a03d78f
Simon H-M <simonhm31@gmail.com>
2024-05-30 16:57:32 -0500

    sdv

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -1,11 +1,9 @@
 //Function to multiply two numbers
-let multiplyTwoNumbers1 = function(a, b){
-    return (a * b);
-}
+let multiplyTwoNumbers1 = function (a, b) {
+  return a * b;
+};
 console.log(multiplyTwoNumbers1(2, 3)); //Returns 6
 
-
 //Arrow function to multiply two numbers
-let multiplyTwoNumbers2 = (a, b) => (a * b);
+let multiplyTwoNumbers2 = (a, b) => a * b;
 console.log(multiplyTwoNumbers2(3, 2)); //Returns 6
-
